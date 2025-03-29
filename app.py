d4290dd9c1a42c5c68c38ee40c64556dd4764fef
ataidejr07 <ataidecarvalho.jr7@gmail.com>
2025-03-28 22:53:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,11 +19,9 @@ with st.sidebar:
     
     # Cartão "Cartões Espelho"
     if st.markdown('<div class="card">📋 Cartões Espelho</div>', unsafe_allow_html=True):
-        st.write("Acessando Cartões Espelho")
     
     # Cartão "Laudos e Receitas"
     if st.markdown('<div class="card">📝 Laudos e Receitas</div>', unsafe_allow_html=True):
-        st.write("Acessando Laudos e Receitas")
 
 # Organização dos botões principais em seções
 
@@ -35,17 +33,14 @@ col1, col2, col3 = st.columns(3)
 # Coluna 1
 with col1:
     if st.markdown('<div class="card">🏠 Domicílios</div>', unsafe_allow_html=True):
-        st.write("Acessando Domicílios")
 
 # Coluna 2
 with col2:
     if st.markdown('<div class="card">👨‍👩‍👧 Famílias</div>', unsafe_allow_html=True):
-        st.write("Acessando Famílias")
 
 # Coluna 3
 with col3:
     if st.markdown('<div class="card">🧑 Cidadãos</div>', unsafe_allow_html=True):
-        st.write("Acessando Cidadãos")
 
 st.subheader("Análises e Relatórios")
 
@@ -55,14 +50,11 @@ col1, col2, col3 = st.columns(3)
 # Coluna 1
 with col1:
     if st.markdown('<div class="card">📊 Relatórios</div>', unsafe_allow_html=True):
-        st.write("Acessando Relatórios")
 
 # Coluna 2
 with col2:
     if st.markdown('<div class="card">📈 Resumo de Produção</div>', unsafe_allow_html=True):
-        st.write("Acessando Resumo de Produção")
 
 # Coluna 3
 with col3:
     if st.markdown('<div class="card">👶⚰️ Nascimentos e Óbitos</div>', unsafe_allow_html=True):
-        st.write("Acessando Nascimentos e Óbitos")
